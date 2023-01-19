eed63a89b518e3cbf76d0d1d99ede00da6d01d06
Marsela Sulku <msulku15@gmail.com>
2023-01-18 16:52:15 -0800

    sign in w google working

--- src/App.js
+++ src/App.js
@@ -1,50 +1,35 @@
 import { useEffect, useState } from 'react';
-import jwt_decode from 'jwt-decode';
-
-import { google } from 'googleapis';
 
 import './App.css';
 
-
 function App() {  
-  const [user, setUser] = useState({});
-
+  const [client, setClient] = useState(null);
   function handleSignInResponse(response) {
-    console.log("response ", response)
-    // let userObject = jwt_decode(response.credential);
-    // console.log('user', userObject);
-    // setUser(userObject);
+    console.log("response ", response);
+    // use access token to make requests
   };
 
-  // useEffect(() => {
-  //   /* global google */ 
-  //   google.accounts.id.initialize({
-  //     client_id: '88893072292-5nurm0kr7lnfcp27vm4a2ge86e6fff5p.apps.googleusercontent.com',
-  //     callback: handleSignInResponse
-  //   });
-
-  //   google.accounts.id.renderButton(
-  //     document.getElementById("signInDiv"),
-  //     { theme: "outline", size: "large" }
-  //   );
-
-  //   google.accounts.id.prompt();
-  // }, [])
-
-
   useEffect(() => {
-    const client = google.accounts.oauth2.initTokenClient({
-      client_id: 'YOUR_GOOGLE_CLIENT_ID',
-      scope: 'https://www.googleapis.com/auth/calendar.readonly',
+    /* global google */
+    const c = google.accounts.oauth2.initTokenClient({
+      client_id: '88893072292-5nurm0kr7lnfcp27vm4a2ge86e6fff5p.apps.googleusercontent.com',
+      scope: 'https://www.googleapis.com/auth/youtube.readonly \
+      https://www.googleapis.com/auth/yt-analytics.readonly',
       callback: handleSignInResponse,
     });
+
+    setClient(c);
   }, []);
+
 
   return (
     <div className="App">
-      <div id="signInDiv"></div>
-      <button onclick="client.requestAccessToken();">Authorize me</button>
-    </div>
+
+      { client ? 
+        <button type="button" class="login-with-google-btn" onClick={() => client.requestAccessToken()}>Sign in with Google</button> :
+        null 
+      }
+    </div> 
   );
 }
 
